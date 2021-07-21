3fb36422cece2b9aeabb2362a5bc9126733abc10
Arjun Doel <arjundoel@gmail.com>
2021-07-21 15:16:03 +0100

    long & lat decimal fix

--- locations/models.py
+++ locations/models.py
@@ -5,15 +5,17 @@ class Location(models.Model):
     image1 = models.CharField(max_length=500, blank=True)
     image2 = models.CharField(max_length=500, blank=True)
     image3 = models.CharField(max_length=500, blank=True)
-    description = models.CharField(max_length=200, default=None)
+    description = models.TextField(max_length=200, default=None)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, default=None, blank=True)
     postal_code = models.CharField(max_length=100, default=None)
     country = models.CharField(max_length=100)
-    longitude = models.FloatField(default=None, blank=True)
-    lattiude = models.FloatField(default=None, blank=True)
+    latitude = models.DecimalField(max_digits=18, decimal_places=10, default=0, blank=True)
+    longitude = models.DecimalField(max_digits=18, decimal_places=10, default=0, blank=True)
     post_at = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return f"{self.name} - {self.city}"
     
     
