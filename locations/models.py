5d580be22bd9aaa7c42ca3c74849257437d47772
Arjun Doel <arjundoel@gmail.com>
2021-07-21 14:33:51 +0100

    basic location model created

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    image1 = models.CharField(max_length=500, blank=True)
+    image2 = models.CharField(max_length=500, blank=True)
+    image3 = models.CharField(max_length=500, blank=True)
+    description = models.CharField(max_length=200, default=None)
+    post_at = models.CharField(auto_created=True)
+    
+    
+    
