f2fdc11a521f03fce5ac9afa1d32a4997812dc00
Arjun Doel <arjundoel@gmail.com>
2021-07-21 14:53:26 +0100

    location model skeleton

--- locations/models.py
+++ locations/models.py
@@ -6,7 +6,14 @@ class Location(models.Model):
     image2 = models.CharField(max_length=500, blank=True)
     image3 = models.CharField(max_length=500, blank=True)
     description = models.CharField(max_length=200, default=None)
-    post_at = models.CharField(auto_created=True)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, null=True)
+    postal_code = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=100)
+    longitude = models.FloatField(default=None, blank=True)
+    lattiude = models.FloatField(default=None, blank=True)
+    post_at = models.DateTimeField(auto_now_add=True)
     
     
     
