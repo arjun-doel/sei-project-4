0a029d7ff406feeb60174815271bd581cf7b49d5
Arjun Doel <arjundoel@gmail.com>
2021-07-21 21:12:31 +0100

    basic post request complete

--- locations/views.py
+++ locations/views.py
@@ -1,14 +1,28 @@
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Location
 from .serializer import LocationSerializer
 
+
 class LocationListView(APIView):
-    
+
     def get(self, _request):
         location = Location.objects.all()
         serializers_locations = LocationSerializer(location, many=True)
         return Response(serializers_locations.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        # Grab data here
+        location_to_before = request.data
+        # Add geocode here
+        location_to_after = LocationSerializer(data=location_to_before)
+        if location_to_after.is_valid():
+            location_to_after.save()
+            return Response(location_to_after.data, status=status.HTTP_201_CREATED)
+        return Response(location_to_after.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
     
