6a445ecad826ad967989c2cbc667a00febf25c0a
Arjun Doel <arjundoel@gmail.com>
2021-07-22 10:50:03 +0100

    comments model complete

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Comment(models.Model):
     text = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey
+    location = models.ForeignKey(
+        "locations.Location",
+        related_name="comments",
+        on_delete=models.CASCADE
+    )
