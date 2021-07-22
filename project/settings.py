e495ec553b0d094010314c66e72a6c287c00dd2a
Arjun Doel <arjundoel@gmail.com>
2021-07-22 10:46:50 +0100

    geopy & comments-app added to INSTALLED_APPS in settings

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'locations'
+    'geopy',
+    'locations',
+    'comments'
 ]
 
 MIDDLEWARE = [
