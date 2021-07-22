faa8402fc5166917c0eecffa155e679d259f871f
Arjun Doel <arjundoel@gmail.com>
2021-07-22 12:15:58 +0100

    authentication start, pyjwt installed

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'geopy',
     'locations',
-    'comments'
+    'comments',
+    'jwt_auth'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
