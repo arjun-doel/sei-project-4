2195f1d8890e9a8a702fd6785547a7a0c87cca10
Arjun Doel <arjundoel@gmail.com>
2021-07-22 13:25:39 +0100

    migartions fixed, recreated dband remigrated

--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'jwt_auth.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'jwt_auth.authentication.JWTAuthentication'
+    ],
+}
