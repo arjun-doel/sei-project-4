0abc15578d0b2fb03767d1af93dfb5f2b479fbd8
Arjun Doel <arjundoel@gmail.com>
2021-07-23 10:42:29 +0100

    ratings keys added

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class Comment(models.Model):
+    RATING_CHOICES = (
+        (1),
+        (2),
+        (3),
+        (4),
+        (5),
+    )
+    
     text = models.TextField(max_length=300)
+    rating = models.IntegerChoices(choices=RATING_CHOICES, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     location = models.ForeignKey(
         "locations.Location",
