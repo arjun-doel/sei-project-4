e6bc8ef36cfa676487b09d5620b3d02e87a2b31d
Arjun Doel <arjundoel@gmail.com>
2021-07-23 11:35:24 +0100

    front end setup and api routes fixedin backend

--- Backend/project/urls.py
+++ Backend/project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/maps/', include('locations.urls')),
     path('api/comments/', include('comments.urls')),
     path('api/auth/', include('jwt_auth.urls'))
