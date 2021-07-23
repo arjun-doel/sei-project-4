f1335907462917ffaa05c63cfc634a359744da5e
Arjun Doel <arjundoel@gmail.com>
2021-07-23 18:12:24 +0100

    useeffect succusfully collecting data from backend

--- client/src/components/maps/MapIndex.js
+++ client/src/components/maps/MapIndex.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const MapIndex = () => {
 
+  const [locations, setLocations] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/maps')
+        setLocations(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [])
+
+  console.log(locations)
 
   return (
     <h1>Maps</h1>
