89b2c5f3b97a44fa9c2b657445068e22fb389ed6
Arjun Doel <arjundoel@gmail.com>
2021-07-28 20:53:17 +0100

    favourites setup with react reveal aniimations

--- client/src/components/maps/user/Favourites.js
+++ client/src/components/maps/user/Favourites.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import UserNav from './UserNav'
+import Fade from 'react-reveal/Fade'
+import Rating from 'react-rating'
 
 const Favourites = () => {
-  // const [locationsData, setLocationsData] = useState([])
+  const [locationsData, setLocationsData] = useState([])
+  const [saved, setSaved] = useState(false)
 
 
   const getItem = () => {
@@ -13,13 +16,12 @@ const Favourites = () => {
   }
 
   useEffect(() => {
-    console.log('loads',getItem())
     const getData = async () => {
       try {
         const { data } = await axios.get('/api/maps')
-        console.log('data',typeof(data[0].id)) 
+        // console.log('data', typeof (data[0].id))
         const comparison = data.filter(ite => getItem().includes(parseFloat(ite.id)))
-        console.log('f', comparison)
+        setLocationsData(comparison)
       } catch (err) {
         console.log(err)
       }
@@ -27,10 +29,54 @@ const Favourites = () => {
     getData()
   }, [])
 
+  const averageRating = () => {
+    const commentsArray = locationsData.map(ite => ite.comments)
+    console.log(typeof (locationsData[0].comments[0].rating))
+    console.log('com array->', commentsArray[2])
+    const average = commentsArray.reduce((acc, currentValue) => {
+      return acc + currentValue.rating / 5
+    }, 0)
+    console.log(average)
+    return Math.ceil(average)
+  }
+
+  const sortFavourites = () => {
+    setSaved(!saved)
+  }
+
+
+
 
   return (
     <>
       <UserNav />
+      <div className="favourites-wrapper">
+        <Fade top>
+          <h1>your favourites...</h1>
+        </Fade>
+        {locationsData.map(ite =>
+          <Fade key={ite.id} top>
+            <div className="favourites-single">
+              <div className="fav-header">
+                <h3>{ite.name}</h3>
+                {saved ? <i id={ite.id} onClick={sortFavourites} className="far fa-heart"></i> : <i id={ite.id} onClick={sortFavourites} className="fas fa-heart"></i>}
+              </div>
+              <div className="fav-short-text">
+                <Rating
+                  emptySymbol="far fa-star"
+                  fullSymbol="fas fa-star"
+                  readonly
+                  className="rating-header"
+                  initialRating={averageRating()}
+                />
+              </div>
+              <div className="fav-descript">
+                <p>{ite.description}</p>
+              </div>
+            </div>
+          </Fade>
+        )}
+      </div>
     </>
   )
 }
