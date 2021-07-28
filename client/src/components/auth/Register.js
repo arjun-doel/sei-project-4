4c4e0ea9d0820005ed43bf392ff5fcc2abee174a
Arjun Doel <arjundoel@gmail.com>
2021-07-28 07:42:07 +0100

    state added to collect register form data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationHome from '../home/NavigationHome'
 
 const Register = () => {
 
   //* Form object state
-  // const [formData, setFormData] = useState({
-  //   username: '',
-  //   email: '',
-  //   password: '',
-  //   passwordConfirmation: '',
-  // })
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    profile_image: '',
+    password: '',
+    passwordConfirmation: '',
+  })
 
   // const [errors, setErrors] = useState({
   //   username: '',
@@ -21,12 +24,14 @@ const Register = () => {
   // const [newSubmitForm, setNewSubmitForm] = useState([])
 
   //*Get userInput
-  // const handleUserData = e => {
-  //   const getUserData = { ...formData, [e.target.name]: e.target.value }
-  //   const newErrors = { ...errors, [e.target.name]: '' }
-  //   setFormData(getUserData)
-  //   setErrors(newErrors)
-  // }
+  const handleUserData = e => {
+    const getUserData = { ...formData, [e.target.name]: e.target.value }
+    // const newErrors = { ...errors, [e.target.name]: '' }
+    setFormData(getUserData)
+    // setErrors(newErrors)
+  }
+
+  console.log(formData)
 
 
   //* Submit form as post request to backend
@@ -42,33 +47,33 @@ const Register = () => {
 
           <div className="form-group-register">
             <label className="frm-label" htmlFor="email">username</label>
-            <input className="main-input" type="text" name="username" placeholder="enter username" required />
+            <input className="main-input" type="text" name="username" placeholder="enter username" value={formData.username} onChange={handleUserData} required />
           </div>
           <div className="form-group-register">
             <label className="frm-label" htmlFor="email">email</label>
-            <input className="main-input" type="email" name="email" placeholder="enter email" required />
+            <input className="main-input" type="email" name="email" placeholder="enter email" value={formData.email} onChange={handleUserData} required />
           </div>
           <div className="row-register">
             <div className="form-group-register">
               <label className="frm-label" htmlFor="first_name">first name</label>
-              <input className="main-input" type="text" name="first_name" placeholder="enter first name" required />
+              <input className="main-input" type="text" name="first_name" placeholder="enter first name" value={formData.first_name} onChange={handleUserData} required />
             </div>
             <div className="form-group-register">
               <label className="frm-label" htmlFor="last_name">email</label>
-              <input className="main-input" type="text" name="last_name" placeholder="enter last name" required />
+              <input className="main-input" type="text" name="last_name" placeholder="enter last name" value={formData.last_name} onChange={handleUserData} required />
             </div>
           </div>
           <div className="form-group-register">
             <label className="frm-label" htmlFor="password">password</label>
-            <input className="main-input" type="password" name="password" placeholder="enter password" required />
+            <input className="main-input" type="password" name="password" placeholder="enter password" value={formData.password} onChange={handleUserData} required />
           </div>
           <div className="form-group-register">
             <label className="frm-label" htmlFor="password_confirmation">password confirmation</label>
-            <input className="main-input" type="password" name="password_confirmation" placeholder="confirm password" required />
+            <input className="main-input" type="password" name="password_confirmation" placeholder="confirm password" value={formData.password_confirmation} onChange={handleUserData} required />
           </div>
           <div className="form-group-register">
-            <label className="frm-label" htmlFor="profile_photo">upload profile photo</label>
-            <input className="file-input" type="file" name="profile_photo" placeholder="choose image" required />
+            <label className="frm-label" htmlFor="profile_image">upload profile photo</label>
+            <input className="file-input" type="file" name="profile_image" placeholder="choose image" value={formData.profile_image} onChange={handleUserData} required />
           </div>
           <button>submit</button>
         </form>
