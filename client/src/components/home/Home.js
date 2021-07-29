f001c7ef51fe56f641eba2d95e5fda5b514bc6cf
Arjun Doel <arjundoel@gmail.com>
2021-07-29 23:12:43 +0100

    user file edits

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavigationHome from './NavigationHome'
 import { Parallax } from 'react-parallax'
-// import Fade from 'react-reveal/Fade'
+import Fade from 'react-reveal/Fade'
 
 
 const Home = () => {
@@ -28,17 +28,21 @@ const Home = () => {
 
         <div className="second-hero">
           <img id="" src="https://res.cloudinary.com/dlj1sbbtb/image/upload/c_mfit,h_100,q_50,w_1000/v1627080361/useLocal-app-data/Front-End/Homepage/brick-wall-1834784_o2z4v0.jpg" alt="bakery" />
-          <div className="second-hero-text">
-            <h5 id="hero-title">find hundreds of hiden gems in your local area</h5>
-            <hr />
-          </div>
+          <Fade right>
+            <div className="second-hero-text">
+              <h5 id="hero-title">find hundreds of hiden gems in your local area</h5>
+              <hr />
+            </div>
+          </Fade>
         </div>
 
         <div className="second-hero">
-          <div className="second-hero-text">
-            <h5 id="hero-title">share your experiences on our social platform</h5>
-            <hr />
-          </div>
+          <Fade left>
+            <div className="second-hero-text">
+              <h5 id="hero-title">share your experiences on our social platform</h5>
+              <hr />
+            </div>
+          </Fade>
           <img src="https://res.cloudinary.com/dlj1sbbtb/image/upload/c_mfit,q_49,w_1000/v1627048654/useLocal-app-data/Front-End/Homepage/search-4700433_yjzp64.jpg" alt="people at town" />
         </div>
       </div>
