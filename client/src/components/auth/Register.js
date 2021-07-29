47f0a6d46be4ef664d2ea0e56fae4076f324cba0
Arjun Doel <arjundoel@gmail.com>
2021-07-29 21:53:06 +0100

    register page edited

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router'
 import NavigationHome from '../home/NavigationHome'
 import { ImageUploadField } from '../hooks/ImageUploadField'
 import { ToastContainer, toast } from 'react-toastify'
+import Fade from 'react-reveal'
 
 const Register = () => {
   const history = useHistory()
@@ -69,42 +70,64 @@ const Register = () => {
       <div className="register-wrap">
         <form onSubmit={submitForm} className="register-form">
 
-          <div className="form-group-register">
-            <label className="frm-label" htmlFor="email">username</label>
-            <input className="main-input" type="text" name="username" placeholder="enter username" value={formData.username} onChange={handleUserData} required />
-          </div>
-          <div className="form-group-register">
-            <label className="frm-label" htmlFor="email">email</label>
-            <input className="main-input" type="email" name="email" placeholder="enter email" value={formData.email} onChange={handleUserData} required />
-          </div>
+          <Fade top>
+            <div className="form-group-register">
+              <label className="frm-label" htmlFor="email">username</label>
+              <input className="main-input" type="text" name="username" placeholder="enter username" value={formData.username} onChange={handleUserData} required />
+            </div>
+          </Fade>
+
+          <Fade bottom>
+            <div className="form-group-register">
+              <label className="frm-label" htmlFor="email">email</label>
+              <input className="main-input" type="email" name="email" placeholder="enter email" value={formData.email} onChange={handleUserData} required />
+            </div>
+          </Fade>
+
           <div className="row-register">
-            <div className="form-group-register">
-              <label className="frm-label" htmlFor="first_name">first name</label>
-              <input className="main-input" type="text" name="first_name" placeholder="enter first name" value={formData.first_name} onChange={handleUserData} required />
-            </div>
-            <div className="form-group-register">
-              <label className="frm-label" htmlFor="last_name">email</label>
-              <input className="main-input" type="text" name="last_name" placeholder="enter last name" value={formData.last_name} onChange={handleUserData} required />
-            </div>
-          </div>
-          <div className="form-group-register">
-            <label className="frm-label" htmlFor="password">password</label>
-            <input className="main-input" type="password" name="password" placeholder="enter password" value={formData.password} onChange={handleUserData} required />
-          </div>
-          <div className="form-group-register">
-            <label className="frm-label" htmlFor="password_confirmation">password confirmation</label>
-            <input className="main-input" type="password" name="password_confirmation" placeholder="confirm password" value={formData.password_confirmation} onChange={handleUserData} required />
-          </div>
-          <div className="form-group-register">
+            <Fade left>
+              <div className="form-group-register">
+                <label className="frm-label" htmlFor="first_name">first name</label>
+                <input className="main-input" type="text" name="first_name" placeholder="enter first name" value={formData.first_name} onChange={handleUserData} required />
+              </div>
+            </Fade>
 
-            <ImageUploadField 
-              value={formData.profile_image}
-              name="profile_image"
-              handleImageUrl={handleImageUrl}
-            />
+            <Fade right>
+              <div className="form-group-register">
+                <label className="frm-label" htmlFor="last_name">email</label>
+                <input className="main-input" type="text" name="last_name" placeholder="enter last name" value={formData.last_name} onChange={handleUserData} required />
+              </div>
+            </Fade>
 
           </div>
-          <button>submit</button>
+          <Fade top>
+            <div className="form-group-register">
+              <label className="frm-label" htmlFor="password">password</label>
+              <input className="main-input" type="password" name="password" placeholder="enter password" value={formData.password} onChange={handleUserData} required />
+            </div>
+          </Fade>
+
+          <Fade bottom>
+            <div className="form-group-register">
+              <label className="frm-label" htmlFor="password_confirmation">password confirmation</label>
+              <input className="main-input" type="password" name="password_confirmation" placeholder="confirm password" value={formData.password_confirmation} onChange={handleUserData} required />
+            </div>
+          </Fade>
+
+          <Fade top>
+            <div className="form-group-register">
+              <ImageUploadField
+                value={formData.profile_image}
+                name="profile_image"
+                handleImageUrl={handleImageUrl}
+                labelName="choose profile pic"
+              />
+            </div>
+          </Fade>
+
+          <Fade bottom>
+            <button>submit</button>
+          </Fade>
         </form>
       </div>
     </>
