16f1921bb44719bc7041066006d46d6828573b4c
Arjun Doel <arjundoel@gmail.com>
2021-08-03 11:26:27 +0100

    form load image animation added:

--- client/src/components/hooks/ImageUploadField.js
+++ client/src/components/hooks/ImageUploadField.js
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import loading from './load-red.svg'
 
 const uploadUrl = process.env.REACT_APP_CLOUDINARY_URL
 const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
 
 
 export const ImageUploadField = ({ handleImageUrl, value, labelName, nameOf }) => {
+  const [load, setLoad] = useState(false)
 
   const handleUpload = async event => {
+    setLoad(true)
     const data = new FormData()
     data.append('file', event.target.files[0])
     data.append('upload_preset', uploadPreset)
@@ -22,9 +25,10 @@ export const ImageUploadField = ({ handleImageUrl, value, labelName, nameOf }) =
     <>
       <div className="upload-wrapper">
         <label className="frm-label" htmlFor={nameOf}> {labelName}</label>
+        {load && !value ? <img className="load-symbol" src={loading} /> : ''}
         {value ? <i className="fas fa-check-circle"></i> : ''}
+        <input type="file" id="file" name={nameOf} placeholder="choose image" onChange={handleUpload} className="image-upload"/>
       </div>
-      <input className="file-input" type="file" name={nameOf} placeholder="choose image" onChange={handleUpload} />
-    </>   
+    </>
   )
 }
